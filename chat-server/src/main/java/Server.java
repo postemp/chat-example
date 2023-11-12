ce1b474eec01145c8dd99f3fedbb6815ff725f56
postemp <apostemp@mail.ru>
2023-11-12 20:58:36 +0300

    final_project

--- chat-server/src/main/java/Server.java
+++ chat-server/src/main/java/Server.java
@@ -4,10 +4,9 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Server {
-    private int port;
+    final private int port;
     private List<ClientHandler> clients;
 
     private final AuthenticationProvider authenticationProvider;
@@ -20,46 +19,51 @@ public class Server {
 
     private boolean toExit = false;
 
+    private final Object mon = new Object();
+
     public void serverShutdown() {
         this.toExit = true;
 
         new Thread(() -> {
             try {
-                Iterator<ClientHandler> clientHandlerIterator = clients.iterator();
-                while (clientHandlerIterator.hasNext()) {
-                    ClientHandler clientHandler = clientHandlerIterator.next();
-                    clientHandler.sendMessage("Сервер отключается, все на выход!");
-                    System.out.println("Отключаем пользователя " + clientHandler.getUsername());
-                    Socket socket = clientHandler.getSocket();
-                    DataInputStream in = clientHandler.getIn();
-                    DataOutputStream out = clientHandler.getOut();
-                    if (in != null) {
-                        try {
-                            in.close();
-                        } catch (IOException e) {
-                            System.out.println("in exception ");
-                            throw new RuntimeException(e);
+                synchronized (mon) {
+                    Iterator<ClientHandler> clientHandlerIterator = clients.iterator();
+                    while (clientHandlerIterator.hasNext()) {
+                        ClientHandler clientHandler = clientHandlerIterator.next();
+                        clientHandler.sendMessage("Сервер отключается, все на выход!");
+                        System.out.println("Отключаем пользователя " + clientHandler.getUsername());
+                        Socket socket = clientHandler.getSocket();
+                        DataInputStream in = clientHandler.getIn();
+                        DataOutputStream out = clientHandler.getOut();
+                        if (in != null) {
+                            try {
+                                in.close();
+                            } catch (IOException e) {
+                                System.out.println("in exception ");
+                                throw new RuntimeException(e);
+                            }
                         }
-                    }
-                    if (out != null) {
-                        try {
-                            out.close();
-                        } catch (IOException e) {
-                            System.out.println("out exception ");
-                            throw new RuntimeException(e);
-                        }
-                    }
-
-                    if (socket != null) {
-                        try {
-                            socket.close();
-                        } catch (IOException e) {
-                            System.out.println("socket exception ");
-                            throw new RuntimeException(e);
+                        if (out != null) {
+                            try {
+                                out.close();
+                            } catch (IOException e) {
+                                System.out.println("out exception ");
+                                throw new RuntimeException(e);
+                            }
                         }
 
+                        if (socket != null) {
+                            try {
+                                socket.close();
+                            } catch (IOException e) {
+                                System.out.println("socket exception ");
+                                throw new RuntimeException(e);
+                            }
+
+                        }
+                        clientHandlerIterator.remove();
                     }
-                    clientHandlerIterator.remove();
+                    mon.notifyAll(); // будим поток в методе start(), что бы быстрее вышел из цикла
                 }
             } catch (Exception e) {
                 System.out.println("Exception:" + e);
@@ -73,6 +77,7 @@ public class Server {
             System.out.println("exception during closing serverSocket");
             throw new RuntimeException(e);
         }
+
     }
 
     public Server(int port, AuthenticationProvider authenticationProvider) {
@@ -86,53 +91,55 @@ public class Server {
             serverSocket = new ServerSocket(port);
             new Thread(() -> {
                 try {
-                    while (!toExit) {
-                        // высчитываем разницу между временем логирования и текущим в минутах и отключаем клиента, если больше 20 мин.
-                        Date currentDate = new Date();
+                    synchronized (mon) {
+                        while (!toExit) {
+                            // высчитываем разницу между временем логирования и текущим в минутах и отключаем клиента, если больше 20 мин.
+                            Date currentDate = new Date();
 //                        System.out.println("begin----------------------------" + currentDate);
-                        long diffInMillies = 0;
-                        Iterator<ClientHandler> clientHandlerIterator = clients.iterator();
-                        while (clientHandlerIterator.hasNext()) {
-                            ClientHandler clientHandler = clientHandlerIterator.next();
-                            diffInMillies = Math.abs(currentDate.getTime() - clientHandler.getLoginDate().getTime()) / 60000; // don't forget to change  to 60000
-                            if (diffInMillies >= 20) {
+                            long diffInMillies = 0;
+                            Iterator<ClientHandler> clientHandlerIterator = clients.iterator();
+                            while (clientHandlerIterator.hasNext()) {
+                                ClientHandler clientHandler = clientHandlerIterator.next();
+                                diffInMillies = Math.abs(currentDate.getTime() - clientHandler.getLoginDate().getTime()) / 60000; // don't forget to change  to 60000
+                                if (diffInMillies >= 20) {
 //                            if (clientHandler.getUsername().equals("Pasha") && diffInMillies >= 1) {
-                                System.out.println("Пользовалель " + clientHandler.getUsername() + " находился в чате в течение " + Long.toString(diffInMillies) + " min");
-                                clientHandler.sendMessage("Ну нельзя так долго сидеть в чате, идите работать! :)");
-                                System.out.println("Отключаем пользователя " + clientHandler.getUsername());
-                                Socket socket = clientHandler.getSocket();
-                                DataInputStream in = clientHandler.getIn();
-                                DataOutputStream out = clientHandler.getOut();
-                                if (in != null) {
-                                    try {
-                                        in.close();
-                                    } catch (IOException e) {
-                                        System.out.println("in exception ");
-                                        throw new RuntimeException(e);
+                                    System.out.println("Пользовалель " + clientHandler.getUsername() + " находился в чате в течение " + Long.toString(diffInMillies) + " min");
+                                    clientHandler.sendMessage("Ну нельзя так долго сидеть в чате, идите работать! :)");
+                                    System.out.println("Отключаем пользователя " + clientHandler.getUsername());
+                                    Socket socket = clientHandler.getSocket();
+                                    DataInputStream in = clientHandler.getIn();
+                                    DataOutputStream out = clientHandler.getOut();
+                                    if (in != null) {
+                                        try {
+                                            in.close();
+                                        } catch (IOException e) {
+                                            System.out.println("in exception ");
+                                            throw new RuntimeException(e);
+                                        }
                                     }
-                                }
-                                if (out != null) {
-                                    try {
-                                        out.close();
-                                    } catch (IOException e) {
-                                        System.out.println("out exception ");
-                                        throw new RuntimeException(e);
-                                    }
-                                }
-
-                                if (socket != null) {
-                                    try {
-                                        socket.close();
-                                    } catch (IOException e) {
-                                        System.out.println("socket exception ");
-                                        throw new RuntimeException(e);
+                                    if (out != null) {
+                                        try {
+                                            out.close();
+                                        } catch (IOException e) {
+                                            System.out.println("out exception ");
+                                            throw new RuntimeException(e);
+                                        }
                                     }
 
+                                    if (socket != null) {
+                                        try {
+                                            socket.close();
+                                        } catch (IOException e) {
+                                            System.out.println("socket exception ");
+                                            throw new RuntimeException(e);
+                                        }
+
+                                    }
+                                    clientHandlerIterator.remove();
                                 }
-                                clientHandlerIterator.remove();
                             }
+                            mon.wait(60000); // 60000
                         }
-                        Thread.sleep(60000); // 60000
                     }
                 } catch (Exception e) {
                     System.out.println("Exception:" + e);
@@ -151,8 +158,7 @@ public class Server {
                 }
                 new ClientHandler(socket, this, serverSocket);
             }
-        } catch (
-                IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
         } finally {
